964b2fa5c9b2ef0fef97eb3bce60feb8435f2cf8
alebcj <ale.zammi@gmail.com>
2018-05-23 18:20:42 -0300

    Subo correciones para creacion de Tablas

--- script_creacion_inicial.sql
+++ script_creacion_inicial.sql
@@ -21,9 +21,23 @@ CREATE TABLE BIG_DATA.Regimen (
 )
 
 
+CREATE TABLE BIG_DATA.PaisNacionalidad (
+	idPaisNacionalidad NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
+	pais NVARCHAR (50),
+	nacionalidad NVARCHAR (50),
+	PRIMARY KEY (idPaisNacionalidad)
+)
+
+CREATE TABLE BIG_DATA.TipoDocumento (
+	idDocumento NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
+	tipoDocumentoDesc NVARCHAR (5),
+	PRIMARY KEY (idDocumento)
+)
+
+
 CREATE TABLE BIG_DATA.Rol (
 	idRol NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
-	nombreRol NVARCHAR (255),
+	rolDesc NVARCHAR (255),
 	PRIMARY KEY (idRol)
 )
 
@@ -46,136 +60,141 @@ CREATE TABLE BIG_DATA.Usuario (
 	username NVARCHAR (255) NOT NULL,
 	userpassword NVARCHAR (20),
 	idRol NUMERIC (18,0),
-	nombre nvarchar (255),
-	apellido nvarchar (255),
+	nombre NVARCHAR (255),
+	apellido NVARCHAR (255),
 	tipo_Documento NUMERIC (18,0),
-	documento numeric (18,0),
-	mail nvarchar (255),
-	telefono numeric (18,0),
-	direccion nvarchar (255),
-	fecha_Nacimiento datetime,
+	documento NUMERIC (18,0),
+	mail NVARCHAR (255),
+	telefono NUMERIC (18,0),
+	direccion NVARCHAR (255),
+	fecha_Nacimiento DATETIME,
 	PRIMARY KEY (username),
 	FOREIGN KEY (idRol) REFERENCES BIG_DATA.Rol (idRol),
 	FOREIGN KEY (tipo_documento) REFERENCES BIG_DATA.TipoDocumento (idDocumento)
 )
 
 CREATE TABLE BIG_DATA.TipoHabitacion (
-	idTipo numeric (18,0) IDENTITY (1006,1) NOT NULL,
-	descripcion nvarchar (255),
-	precio numeric(18,2),
-	PRIMARY KEY (idTipo)
+	idTipoHabitacion NUMERIC (18,0) IDENTITY (1006,1) NOT NULL,
+	descripcion NVARCHAR (255),
+	precio NUMERIC(18,2),
+	porcentual numeric(18, 2),
+	PRIMARY KEY (idTipoHabitacion)
 )
 
 
 CREATE TABLE BIG_DATA.Cliente (
-	idCliente numeric (18,0) IDENTITY (1,1) NOT NULL,
-	nombre nvarchar (255),
-	apellido nvarchar (255),
-	tipo_Documento nvarchar (5),
-	documento numeric (18,0),
-	nacionalidad nvarchar (255),
-	mail nvarchar (255),
-	telefono numeric (18,0),
-	calle nvarchar (255),
-	numero_Calle numeric(18, 0),
-	piso numeric(18,0),
-	departamento nvarchar(50),
-	localidad nvarchar (255),
-	Pais nvarchar (255),
-	fecha_Nacimiento datetime,
-	pasaporte numeric(18,0),
-	PRIMARY KEY (idCliente)
+	idCliente NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
+	nombre NVARCHAR (255) NOT NULL,
+	apellido NVARCHAR (255) NOT NULL,
+	tipo_Documento NUMERIC (18,0) NOT NULL,
+	documento NUMERIC (18,0) NOT NULL,
+	nacionalidad NUMERIC (18,0),
+	mail NVARCHAR (255) NOT NULL,
+	telefono NUMERIC (18,0),
+	calle NVARCHAR (255),
+	numero_Calle NUMERIC(18, 0),
+	piso NUMERIC(18,0),
+	departamento NVARCHAR(50),
+	localidad NVARCHAR (255),
+	paisOrigen NUMERIC (18,0),
+	fecha_Nacimiento DATETIME,
+	pasaporte NUMERIC(18,0),
+	PRIMARY KEY (idCliente),
+	FOREIGN KEY (tipo_documento) REFERENCES BIG_DATA.TipoDocumento (idDocumento),
+	FOREIGN KEY (nacionalidad) REFERENCES BIG_DATA.PaisNacionalidad (idPaisNacionalidad),
+	FOREIGN KEY (paisOrigen) REFERENCES BIG_DATA.PaisNacionalidad (idPaisNacionalidad)
 )
 
 CREATE TABLE BIG_DATA.Habitacion (
 	idHabitacion numeric (18,0) IDENTITY (1,1) NOT NULL,
-	numero numeric (18,0),
-	piso numeric (18,0),
-	vista nvarchar (50),
-	idTipo numeric (18,0) ,
-	comodidades nvarchar (255),
-	porcentual numeric(18, 2),
+	idHotel NUMERIC(18,0) NOT NULL,
+	numeroHab NUMERIC (18,0) NOT NULL,
+	piso NUMERIC (18,0),
+	vistaFrente CHAR (1),
+	idTipo NUMERIC (18,0) ,
+	comodidades NVARCHAR (255),
+	habitacionActiva BIT DEFAULT 1,
+	habitacionOcupada BIT DEFAULT 0,
 	PRIMARY KEY (idHabitacion),
-	FOREIGN KEY (idTipo) REFERENCES BIG_DATA.Tipo
+	FOREIGN KEY (idTipo) REFERENCES BIG_DATA.TipoHabitacion (idTipoHabitacion)
+)
+
+
+CREATE TABLE BIG_DATA.EstadoReserva (
+	idEstadoReserva NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
+	estadoReservaDesc VARCHAR (255),
+	PRIMARY KEY (idEstadoReserva)
 )
 
 CREATE TABLE BIG_DATA.Reserva (
-	idReserva numeric (18,0) IDENTITY (1,1) NOT NULL,
-	codigoReserva numeric(18,0),
-	cantidadNoches numeric (18,0),
-	fecha_Reserva_Realizada datetime,
-	fecha_Reserva_Desde datetime,
-	fecha_Reserva_Hasta datetime,
-	idTipo numeric (18,0),
-	idHotel numeric (18,0),
-	idRegimen numeric (18,0),
-	idHabitacion numeric (18,0),
-	idCliente numeric (18,0),
+	idReserva NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
+	codigoReserva NUMERIC(18,0) NOT NULL,
+	cantidadNoches NUMERIC (18,0),
+	fecha_Reserva_Realizada DATETIME DEFAULT (getdate()),
+	fecha_Reserva_Desde DATETIME NOT NULL,
+	fecha_Reserva_Hasta DATETIME NOT NULL,
+	idTipoHabitacion NUMERIC (18,0) NOT NULL,
+	idHotel NUMERIC (18,0) NOT NULL,
+	idRegimen NUMERIC (18,0),
+	idHabitacion NUMERIC (18,0),
+	idCliente NUMERIC (18,0),
+	idEstadoReserva NUMERIC (18,0),
+	ultimaModificacionPor NVARCHAR (255) DEFAULT (suser_name()),
 	PRIMARY KEY (idReserva),
-	FOREIGN KEY (idTipo) REFERENCES BIG_DATA.Tipo (idTipo),
+	FOREIGN KEY (idTipoHabitacion) REFERENCES BIG_DATA.TipoHabitacion (idTipoHabitacion),
 	FOREIGN KEY (idHotel) REFERENCES BIG_DATA.Hotel (idHotel),
 	FOREIGN KEY (idRegimen) REFERENCES BIG_DATA.Regimen (idRegimen),
 	FOREIGN KEY (idHabitacion) REFERENCES BIG_DATA.Habitacion (idHabitacion),
-	FOREIGN KEY (idCliente) REFERENCES BIG_DATA.Cliente (idCliente)
+	FOREIGN KEY (idCliente) REFERENCES BIG_DATA.Cliente (idCliente),
+	FOREIGN KEY (idEstadoReserva) REFERENCES BIG_DATA.EstadoReserva (idEstadoReserva)
 )
+
 
 
 CREATE TABLE BIG_DATA.Funcion (
-	idFuncion numeric (18,0) IDENTITY (1,1) NOT NULL,
-	funcionalidad nvarchar (255),
-	idRol numeric (18,0),
+	idFuncion NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
+	funcionDesc NVARCHAR (255),
 	PRIMARY KEY (idFuncion),
-	FOREIGN KEY (idRol) REFERENCES BIG_DATA.Rol (idRol)
 )
 
-CREATE TABLE BIG_DATA.Nacionalidad (
-	idNacionalidad numeric (18,0) IDENTITY (1,1) NOT NULL,
-	nacionalidad nvarchar (50),
-	PRIMARY KEY (idNacionalidad)
-)
 
-CREATE TABLE BIG_DATA.TipoDocumento (
-	idDocumento numeric (18,0) IDENTITY (1,1) NOT NULL,
-	nombreTipoDocumento nvarchar (5),
-	PRIMARY KEY (idDocumento)
-)
 
 CREATE TABLE BIG_DATA.CierreHotel (
-	idCierre numeric (18,0) IDENTITY (1,1) NOT NULL,
-	idHotel numeric (18,0),
-	fecha_Inicio datetime,
-	fecha_Fin datetime,
-	descripcion nvarchar (255),
+	idCierre NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
+	idHotel NUMERIC (18,0),
+	fecha_Inicio DATETIME,
+	fecha_Fin DATETIME,
+	descripcion NVARCHAR (255),
 	PRIMARY KEY (idCierre),
 	FOREIGN KEY (idHotel) REFERENCES BIG_DATA.Hotel (idHotel)
 )
 
 CREATE TABLE BIG_DATA.Estadia (
-	idEstadia numeric (18,0) IDENTITY (1,1) NOT NULL,
-	idReserva numeric (18,0),
-	fecha_Check_In datetime,
-	fecha_Check_Out datetime,
-	cantidadDias numeric (18,0),
+	idEstadia NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
+	idReserva NUMERIC (18,0),
+	fecha_Check_In DATETIME,
+	fecha_Check_Out DATETIME,
+	cantidadDias NUMERIC (18,0),
 	PRIMARY KEY (idEstadia),
 	FOREIGN KEY (idReserva) REFERENCES BIG_DATA.Reserva (idReserva)
 )
 
 CREATE TABLE BIG_DATA.Factura (
-	idFactura numeric (18,0) IDENTITY (1,1) NOT NULL,
-	idEstadia numeric (18,0),
-	numero numeric (18,0),
-	fecha datetime,
-	total money,
+	idFactura NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
+	idEstadia NUMERIC (18,0),
+	numero NUMERIC (18,0),
+	fecha DATETIME,
+	total MONEY,
 	PRIMARY KEY (idFactura),
 	FOREIGN KEY (idEstadia) REFERENCES BIG_DATA.Estadia (idEstadia)
 )
 
 CREATE TABLE BIG_DATA.Item (
-	idItem numeric (18,0) IDENTITY (1,1) NOT NULL,
-	idFactura numeric (18,0),
-	cantidad numeric (18,0),
-	monto money,
-	idEstadia numeric (18,0),
+	idItem NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
+	idFactura NUMERIC (18,0),
+	cantidad NUMERIC (18,0),
+	monto MONEY,
+	idEstadia NUMERIC (18,0),
 	PRIMARY KEY (idItem),
 	FOREIGN KEY (idFactura) REFERENCES BIG_DATA.Factura (idFactura),
 	FOREIGN KEY (idEstadia) REFERENCES BIG_DATA.Estadia (idEstadia)
@@ -183,44 +202,44 @@ CREATE TABLE BIG_DATA.Item (
 
 
 CREATE TABLE BIG_DATA.Consumible (
-	idConsumible numeric (18,0) IDENTITY (1,1) NOT NULL,
-	codigo numeric (18,0),
-	consumible varchar (255),
-	precio numeric (18,2),
+	idConsumible NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
+	codigo NUMERIC (18,0),
+	consumible VARCHAR (255),
+	precio NUMERIC (18,2),
 	PRIMARY KEY (idConsumible)
 )
 
 CREATE TABLE BIG_DATA.RegimenXHotel (
-	idRegimenHotel numeric (18,0) IDENTITY (1,1) NOT NULL,
-	idRegimen numeric (18,0),
-	idHotel numeric (18,0),
+	idRegimenHotel NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
+	idRegimen NUMERIC (18,0),
+	idHotel NUMERIC (18,0),
 	PRIMARY KEY (idRegimenHotel),
 	FOREIGN KEY (idRegimen) REFERENCES BIG_DATA.Regimen (idRegimen),
 	FOREIGN KEY (idHotel) REFERENCES BIG_DATA.Hotel (idHotel)
 )
 
 CREATE TABLE BIG_DATA.UsuarioXHotel (
-	idUsuarioHotel numeric (18,0) IDENTITY (1,1) NOT NULL,
-	idHotel numeric (18,0),
-	username nvarchar (255),
+	idUsuarioHotel NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
+	idHotel NUMERIC (18,0),
+	username NVARCHAR (255),
 	PRIMARY KEY (idUsuarioHotel),
 	FOREIGN KEY (idHotel) REFERENCES BIG_DATA.Hotel (idHotel),
 	FOREIGN KEY (username) REFERENCES BIG_DATA.Usuario (username)
 )
 
-CREATE TABLE BIG_DATA.RolXFuncion (
-	idRolFuncion numeric (18,0) IDENTITY (1,1) NOT NULL,
-	idRol numeric (18,0),
-	idFuncion numeric (18,0),
-	PRIMARY KEY (idRolFuncion),
+CREATE TABLE BIG_DATA.FuncionXRol (
+	idFuncionRol NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
+	idRol NUMERIC (18,0),
+	idFuncion NUMERIC (18,0),
+	PRIMARY KEY (idFuncionRol),
 	FOREIGN KEY (idRol) REFERENCES BIG_DATA.Rol (idRol),
 	FOREIGN KEY (idFuncion) REFERENCES BIG_DATA.Funcion (idFuncion)
 )
 
 CREATE TABLE BIG_DATA.ConsumibleXEstadia (
-	idConsumibleEstadia numeric (18,0) IDENTITY (1,1) NOT NULL,
-	idEstadia numeric (18,0),
-	idConsumible numeric (18,0),
+	idConsumibleEstadia NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
+	idEstadia NUMERIC (18,0),
+	idConsumible NUMERIC (18,0),
 	PRIMARY KEY (idConsumibleEstadia),
 	FOREIGN KEY (idEstadia) REFERENCES BIG_DATA.Estadia (idEstadia),
 	FOREIGN KEY (idConsumible) REFERENCES BIG_DATA.Consumible (idConsumible)
