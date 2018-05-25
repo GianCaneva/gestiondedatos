6c71adc4d64c4639a4c9fdef02b2a2c284bae26d
FaridUtn <faridfafu@hotmail.com>
2018-05-25 14:41:27 -0300

    Migracion tabla reserva

--- SUPER SCRIPT V2.sql
+++ SUPER SCRIPT V2.sql
@@ -59,9 +59,12 @@ PRINT @Sql
 ---------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------
-ALTER TABLE BIG_DATA.CierreHotel drop constraint FK__CierreHot__idHot__4AD81681;ALTER TABLE BIG_DATA.Ciudad drop constraint FK__Ciudad__idPaisNa__668030F6;ALTER TABLE BIG_DATA.Cliente drop constraint FK__Cliente__tipo_Do__37C5420D;ALTER TABLE BIG_DATA.Cliente drop constraint FK__Cliente__naciona__38B96646;ALTER TABLE BIG_DATA.Cliente drop constraint FK__Cliente__paisOri__39AD8A7F;ALTER TABLE BIG_DATA.ConsumibleXEstadia drop constraint FK__Consumibl__idEst__62AFA012;ALTER TABLE BIG_DATA.ConsumibleXEstadia drop constraint FK__Consumibl__idCon__63A3C44B;ALTER TABLE BIG_DATA.Estadia drop constraint FK__Estadia__idReser__4DB4832C;ALTER TABLE BIG_DATA.Factura drop constraint FK__Factura__idEstad__5090EFD7;ALTER TABLE BIG_DATA.FuncionXRol drop constraint FK__FuncionXR__idRol__5EDF0F2E;ALTER TABLE BIG_DATA.FuncionXRol drop constraint FK__FuncionXR__idFun__5FD33367;ALTER TABLE BIG_DATA.Habitacion drop constraint FK__Habitacio__idTip__3E723F9C;ALTER TABLE BIG_DATA.Hotel drop constraint FK__Hotel__paisHotel__3118447E;ALTER TABLE BIG_DATA.Item drop constraint FK__Item__idFactura__536D5C82;ALTER TABLE BIG_DATA.Item drop constraint FK__Item__idEstadia__546180BB;ALTER TABLE BIG_DATA.RegimenXHotel drop constraint FK__RegimenXH__idReg__573DED66;ALTER TABLE BIG_DATA.RegimenXHotel drop constraint FK__RegimenXH__idHot__5832119F;ALTER TABLE BIG_DATA.Reserva drop constraint FK__Reserva__idTipoH__4336F4B9;ALTER TABLE BIG_DATA.Reserva drop constraint FK__Reserva__idHotel__442B18F2;ALTER TABLE BIG_DATA.Reserva drop constraint FK__Reserva__idRegim__451F3D2B;ALTER TABLE BIG_DATA.Reserva drop constraint FK__Reserva__idHabit__46136164;ALTER TABLE BIG_DATA.Reserva drop constraint FK__Reserva__idClien__4707859D;ALTER TABLE BIG_DATA.Reserva drop constraint FK__Reserva__idEstad__47FBA9D6;ALTER TABLE BIG_DATA.Usuario drop constraint FK__Usuario__idRol__33F4B129;ALTER TABLE BIG_DATA.Usuario drop constraint FK__Usuario__tipo_Do__34E8D562;ALTER TABLE BIG_DATA.UsuarioXHotel drop constraint FK__UsuarioXH__idHot__5B0E7E4A;ALTER TABLE BIG_DATA.UsuarioXHotel drop constraint FK__UsuarioXH__usern__5C02A283;DROP TABLE BIG_DATA.[CierreHotel];DROP TABLE BIG_DATA.[Ciudad];DROP TABLE BIG_DATA.[Cliente];DROP TABLE BIG_DATA.[Consumible];DROP TABLE BIG_DATA.[ConsumibleXEstadia];DROP TABLE BIG_DATA.[Estadia];DROP TABLE BIG_DATA.[EstadoReserva];DROP TABLE BIG_DATA.[Factura];DROP TABLE BIG_DATA.[Funcion];DROP TABLE BIG_DATA.[FuncionXRol];DROP TABLE BIG_DATA.[Habitacion];DROP TABLE BIG_DATA.[Hotel];DROP TABLE BIG_DATA.[Item];DROP TABLE BIG_DATA.[PaisNacionalidad];DROP TABLE BIG_DATA.[Regimen];DROP TABLE BIG_DATA.[RegimenXHotel];DROP TABLE BIG_DATA.[Reserva];DROP TABLE BIG_DATA.[Rol];DROP TABLE BIG_DATA.[TipoDocumento];DROP TABLE BIG_DATA.[TipoHabitacion];DROP TABLE BIG_DATA.[Usuario];DROP TABLE BIG_DATA.[UsuarioXHotel];
+ALTER TABLE BIG_DATA.CierreHotel drop constraint FK__CierreHot__idHot__7C1A6C5A;ALTER TABLE BIG_DATA.Ciudad drop constraint FK__Ciudad__idPaisNa__5E8A0973;ALTER TABLE BIG_DATA.Cliente drop constraint FK__Cliente__naciona__69FBBC1F;ALTER TABLE BIG_DATA.Cliente drop constraint FK__Cliente__paisOri__6AEFE058;ALTER TABLE BIG_DATA.Cliente drop constraint FK__Cliente__tipo_Do__690797E6;ALTER TABLE BIG_DATA.ConsumibleXEstadia drop constraint FK__Consumibl__idCon__14E61A24;ALTER TABLE BIG_DATA.ConsumibleXEstadia drop constraint FK__Consumibl__idEst__13F1F5EB;ALTER TABLE BIG_DATA.Estadia drop constraint FK__Estadia__idReser__7EF6D905;ALTER TABLE BIG_DATA.Factura drop constraint FK__Factura__idEstad__01D345B0;ALTER TABLE BIG_DATA.FuncionXRol drop constraint FK__FuncionXR__idFun__11158940;ALTER TABLE BIG_DATA.FuncionXRol drop constraint FK__FuncionXR__idRol__10216507;ALTER TABLE BIG_DATA.Habitacion drop constraint FK__Habitacio__idTip__6FB49575;ALTER TABLE BIG_DATA.Hotel drop constraint FK__Hotel__paisHotel__6166761E;ALTER TABLE BIG_DATA.Hotel drop constraint FK__Hotel__idCiudad__625A9A57;ALTER TABLE BIG_DATA.Item drop constraint FK__Item__idEstadia__05A3D694;ALTER TABLE BIG_DATA.Item drop constraint FK__Item__idFactura__04AFB25B;ALTER TABLE BIG_DATA.RegimenXHotel drop constraint FK__RegimenXH__idHot__09746778;ALTER TABLE BIG_DATA.RegimenXHotel drop constraint FK__RegimenXH__idReg__0880433F;ALTER TABLE BIG_DATA.Reserva drop constraint FK__Reserva__idRegim__76619304;ALTER TABLE BIG_DATA.Reserva drop constraint FK__Reserva__idTipoH__74794A92;ALTER TABLE BIG_DATA.Reserva drop constraint FK__Reserva__idEstad__793DFFAF;ALTER TABLE BIG_DATA.Reserva drop constraint FK__Reserva__idHotel__756D6ECB;ALTER TABLE BIG_DATA.Reserva drop constraint FK__Reserva__idClien__7849DB76;ALTER TABLE BIG_DATA.Reserva drop constraint FK__Reserva__idHabit__7755B73D;ALTER TABLE BIG_DATA.Usuario drop constraint FK__Usuario__idRol__65370702;ALTER TABLE BIG_DATA.Usuario drop constraint FK__Usuario__tipo_Do__662B2B3B;ALTER TABLE BIG_DATA.UsuarioXHotel drop constraint FK__UsuarioXH__idHot__0C50D423;ALTER TABLE BIG_DATA.UsuarioXHotel drop constraint FK__UsuarioXH__usern__0D44F85C;DROP TABLE BIG_DATA.[CierreHotel];DROP TABLE BIG_DATA.[Ciudad];DROP TABLE BIG_DATA.[Cliente];DROP TABLE BIG_DATA.[Consumible];DROP TABLE BIG_DATA.[ConsumibleXEstadia];DROP TABLE BIG_DATA.[Estadia];DROP TABLE BIG_DATA.[EstadoReserva];DROP TABLE BIG_DATA.[Factura];DROP TABLE BIG_DATA.[Funcion];DROP TABLE BIG_DATA.[FuncionXRol];DROP TABLE BIG_DATA.[Habitacion];DROP TABLE BIG_DATA.[Hotel];DROP TABLE BIG_DATA.[Item];DROP TABLE BIG_DATA.[PaisNacionalidad];DROP TABLE BIG_DATA.[Regimen];DROP TABLE BIG_DATA.[RegimenXHotel];DROP TABLE BIG_DATA.[Reserva];DROP TABLE BIG_DATA.[Rol];DROP TABLE BIG_DATA.[TipoDocumento];DROP TABLE BIG_DATA.[TipoHabitacion];DROP TABLE BIG_DATA.[Usuario];DROP TABLE BIG_DATA.[UsuarioXHotel];
 
-
+
+
+
+
 
 
 ---------------------------------------------------------------------------------------------------------
@@ -71,284 +74,6 @@ ALTER TABLE BIG_DATA.CierreHotel drop constraint FK__CierreHot__idHot__4AD81681;
 ---------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------
 
-use [GD1C2018]
-GO
-
---Crea el Schema--
-
-IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'BIG_DATA')
-
-BEGIN
-	EXEC ('CREATE SCHEMA BIG_DATA AUTHORIZATION gdHotel2018')
-END
-
---Creacion de tablas--
-
----Tablas sin FK
-CREATE TABLE BIG_DATA.Regimen (
-	idRegimen NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
-	nombre NVARCHAR (255),
-	descripcion NVARCHAR (255),
-	precio MONEY,
-	estado BIT,
-	
-	PRIMARY KEY (idRegimen)
-)
-
-CREATE TABLE BIG_DATA.TipoHabitacion (
-	idTipoHabitacion NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
-	descripcion NVARCHAR (255),
-	precio NUMERIC(18,2),
-	porcentual NUMERIC(18, 2),
-	
-	PRIMARY KEY (idTipoHabitacion)
-)
-
-CREATE TABLE BIG_DATA.PaisNacionalidad (
-	idPaisNacionalidad NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
-	pais NVARCHAR (50),
-	nacionalidad NVARCHAR (50),
-	
-	PRIMARY KEY (idPaisNacionalidad)
-)
-
-CREATE TABLE BIG_DATA.TipoDocumento (
-	idDocumento NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
-	tipoDocumentoDesc NVARCHAR (255),
-	
-	PRIMARY KEY (idDocumento)
-)
-
-
-CREATE TABLE BIG_DATA.Rol (
-	idRol NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
-	rolDesc NVARCHAR (255),
-	
-	PRIMARY KEY (idRol)
-)
-
-CREATE TABLE BIG_DATA.EstadoReserva (
-	idEstadoReserva NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
-	estadoReservaDesc VARCHAR (255),
-	
-	PRIMARY KEY (idEstadoReserva)
-)
-
-CREATE TABLE BIG_DATA.Consumible (
-	idConsumible NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
-	consumibleDesc VARCHAR (255),
-	consumiblePrecio NUMERIC (18,2),
-	
-	PRIMARY KEY (idConsumible)
-)
-
-
-CREATE TABLE BIG_DATA.Funcion (
-	idFuncion NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
-	funcionDesc NVARCHAR (255),
-	
-	PRIMARY KEY (idFuncion),
-)
-
-
-CREATE TABLE BIG_DATA.Ciudad (
-	idCiudad NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
-	idPaisNacionalidad NUMERIC (18,0) NOT NULL,
-	ciudadNombre NVARCHAR (255) NOT NULL,
-
-	PRIMARY KEY (idCiudad),
-	FOREIGN KEY (idPaisNacionalidad) REFERENCES BIG_DATA.PaisNacionalidad
-	)
-
-
-CREATE TABLE BIG_DATA.Hotel (
-	idHotel NUMERIC(18,0) IDENTITY (1,1) NOT NULL,
-	nombreHotel NVARCHAR (255),
-	mailHotel NVARCHAR (255),
-	telefonoHotel NUMERIC (18,0),
-	direccionHotel NVARCHAR(255) ,
-	cantidadDeEstrellas NUMERIC (18,0),
-	idCiudad NUMERIC (18,0),
-	numeroCalle NUMERIC (18,0),
-	paisHotel NUMERIC (18,0),
-	fechaDeCreacion DATETIME,
-	recargaEstrella NUMERIC (18,0)
-	
-	PRIMARY KEY(idHotel),
-	FOREIGN KEY (paisHotel) REFERENCES [BIG_DATA].[PaisNacionalidad] (idPaisNacionalidad),
-	FOREIGN KEY (idCiudad) REFERENCES [BIG_DATA].[Ciudad] (idCiudad)
-)
-
-CREATE TABLE BIG_DATA.Usuario (
-	username NVARCHAR (255) NOT NULL,
-	userpassword NVARCHAR (20),
-	idRol NUMERIC (18,0),
-	nombre NVARCHAR (255),
-	apellido NVARCHAR (255),
-	tipo_Documento NUMERIC (18,0),
-	documento NUMERIC (18,0),
-	mail NVARCHAR (255),
-	telefono NUMERIC (18,0),
-	direccion NVARCHAR (255),
-	fecha_Nacimiento DATETIME,
-	
-	PRIMARY KEY (username),
-	FOREIGN KEY (idRol) REFERENCES BIG_DATA.Rol (idRol),
-	FOREIGN KEY (tipo_documento) REFERENCES BIG_DATA.TipoDocumento (idDocumento)
-)
-
-
-CREATE TABLE BIG_DATA.Cliente (
-	idCliente NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
-	nombre NVARCHAR (255) NOT NULL,
-	apellido NVARCHAR (255) NOT NULL,
-	tipo_Documento NUMERIC (18,0) NOT NULL,
-	documento NUMERIC (18,0) NOT NULL,
-	nacionalidad NUMERIC (18,0),
-	mail NVARCHAR (255) NOT NULL,
-	telefono NUMERIC (18,0),
-	calle NVARCHAR (255),
-	numero_Calle NVARCHAR(255),
-	piso NUMERIC(18,0),
-	departamento NVARCHAR(50),
-	localidad NVARCHAR (255),
-	paisOrigen NUMERIC (18,0),
-	fecha_Nacimiento DATETIME
-	
-	PRIMARY KEY (idCliente),
-	FOREIGN KEY (tipo_documento) REFERENCES BIG_DATA.TipoDocumento (idDocumento),
-	FOREIGN KEY (nacionalidad) REFERENCES BIG_DATA.PaisNacionalidad (idPaisNacionalidad),
-	FOREIGN KEY (paisOrigen) REFERENCES BIG_DATA.PaisNacionalidad (idPaisNacionalidad)
-)
-
-CREATE TABLE BIG_DATA.Habitacion (
-	idHabitacion numeric (18,0) IDENTITY (1,1) NOT NULL,
-	idHotel NUMERIC(18,0) NOT NULL,
-	numeroHab NUMERIC (18,0) NOT NULL,
-	piso NUMERIC (18,0),
-	vistaFrente CHAR (1),
-	idTipo NUMERIC (18,0) ,
-	comodidades NVARCHAR (255),
-	habitacionActiva BIT DEFAULT 1,
-	habitacionOcupada BIT DEFAULT 0,
-
-	PRIMARY KEY (idHabitacion),
-	FOREIGN KEY (idTipo) REFERENCES BIG_DATA.TipoHabitacion (idTipoHabitacion)
-)
-
-
-
-CREATE TABLE BIG_DATA.Reserva (
-	idReserva NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
-	codigoReserva NUMERIC(18,0) NOT NULL,
-	cantidadNoches NUMERIC (18,0),
-	fecha_Reserva_Realizada DATETIME DEFAULT (getdate()),
-	fecha_Reserva_Desde DATETIME,
-	fecha_Reserva_Hasta DATETIME,
-	idTipoHabitacion NUMERIC (18,0) NOT NULL,
-	idHotel NUMERIC (18,0) NOT NULL,
-	idRegimen NUMERIC (18,0),
-	idHabitacion NUMERIC (18,0),
-	idCliente NUMERIC (18,0),
-	idEstadoReserva NUMERIC (18,0),
-	ultimaModificacionPor NVARCHAR (255) DEFAULT (suser_name()),
-
-	PRIMARY KEY (idReserva),
-	FOREIGN KEY (idTipoHabitacion) REFERENCES BIG_DATA.TipoHabitacion (idTipoHabitacion),
-	FOREIGN KEY (idHotel) REFERENCES BIG_DATA.Hotel (idHotel),
-	FOREIGN KEY (idRegimen) REFERENCES BIG_DATA.Regimen (idRegimen),
-	FOREIGN KEY (idHabitacion) REFERENCES BIG_DATA.Habitacion (idHabitacion),
-	FOREIGN KEY (idCliente) REFERENCES BIG_DATA.Cliente (idCliente),
-	FOREIGN KEY (idEstadoReserva) REFERENCES BIG_DATA.EstadoReserva (idEstadoReserva)
-)
-
-
-CREATE TABLE BIG_DATA.CierreHotel (
-	idCierre NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
-	idHotel NUMERIC (18,0),
-	fecha_Inicio DATETIME,
-	fecha_Fin DATETIME,
-	descripcion NVARCHAR (255),
-
-	PRIMARY KEY (idCierre),
-	FOREIGN KEY (idHotel) REFERENCES BIG_DATA.Hotel (idHotel)
-)
-
-CREATE TABLE BIG_DATA.Estadia (
-	idEstadia NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
-	idReserva NUMERIC (18,0),
-	fecha_Check_In DATETIME,
-	fecha_Check_Out DATETIME,
-	cantidadDias NUMERIC (18,0),
-
-	PRIMARY KEY (idEstadia),
-	FOREIGN KEY (idReserva) REFERENCES BIG_DATA.Reserva (idReserva)
-)
-
-CREATE TABLE BIG_DATA.Factura (
-	idFactura NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
-	idEstadia NUMERIC (18,0),
-	numero NUMERIC (18,0),
-	fecha DATETIME,
-	total MONEY,
-
-	PRIMARY KEY (idFactura),
-	FOREIGN KEY (idEstadia) REFERENCES BIG_DATA.Estadia (idEstadia)
-)
-
-CREATE TABLE BIG_DATA.Item (
-	idItem NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
-	idFactura NUMERIC (18,0),
-	cantidad NUMERIC (18,0),
-	monto MONEY,
-	idEstadia NUMERIC (18,0),
-
-	PRIMARY KEY (idItem),
-	FOREIGN KEY (idFactura) REFERENCES BIG_DATA.Factura (idFactura),
-	FOREIGN KEY (idEstadia) REFERENCES BIG_DATA.Estadia (idEstadia)
-)
-
-
-
-CREATE TABLE BIG_DATA.RegimenXHotel (
-	idRegimenHotel NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
-	idRegimen NUMERIC (18,0),
-	idHotel NUMERIC (18,0),
-
-	PRIMARY KEY (idRegimenHotel),
-	FOREIGN KEY (idRegimen) REFERENCES BIG_DATA.Regimen (idRegimen),
-	FOREIGN KEY (idHotel) REFERENCES BIG_DATA.Hotel (idHotel)
-)
-
-CREATE TABLE BIG_DATA.UsuarioXHotel (
-	idUsuarioHotel NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
-	idHotel NUMERIC (18,0),
-	username NVARCHAR (255),
-
-	PRIMARY KEY (idUsuarioHotel),
-	FOREIGN KEY (idHotel) REFERENCES BIG_DATA.Hotel (idHotel),
-	FOREIGN KEY (username) REFERENCES BIG_DATA.Usuario (username)
-)
-
-CREATE TABLE BIG_DATA.FuncionXRol (
-	idFuncionRol NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
-	idRol NUMERIC (18,0),
-	idFuncion NUMERIC (18,0),
-
-	PRIMARY KEY (idFuncionRol),
-	FOREIGN KEY (idRol) REFERENCES BIG_DATA.Rol (idRol),
-	FOREIGN KEY (idFuncion) REFERENCES BIG_DATA.Funcion (idFuncion)
-)
-
-CREATE TABLE BIG_DATA.ConsumibleXEstadia (
-	idConsumibleEstadia NUMERIC (18,0) IDENTITY (1,1) NOT NULL,
-	idEstadia NUMERIC (18,0),
-	idConsumible NUMERIC (18,0),
-
-	PRIMARY KEY (idConsumibleEstadia),
-	FOREIGN KEY (idEstadia) REFERENCES BIG_DATA.Estadia (idEstadia),
-	FOREIGN KEY (idConsumible) REFERENCES BIG_DATA.Consumible (idConsumible)
-)
 
 
 	
@@ -433,6 +158,10 @@ INSERT INTO [BIG_DATA].[Usuario] (username,userpassword,idRol,nombre,apellido,ti
 	('Recepcionista',ENCRYPTBYPASSPHRASE('MOAP','Recepcionista'),2,'NombreRecepcionista','ApellidoRecepcionista', 1, 22222222,'Recepcionista@recepcionista.com',1522222222,'CalleRecepcionista',01/01/1900),	
 	('Guest',ENCRYPTBYPASSPHRASE('MOAP','Guest'),1,'NombreGuest','ApellidoGuest', 3, 33333333,'Guest@guest.com',3333333333,'CalleGuest',01/01/1900)	
 
+--Adicion Valores Tabla Estado Reserva
+INSERT INTO [BIG_DATA].[EstadoReserva] (estadoReservaDesc) values('Reserva Correcta'),('Reserva Modificada'), ('Reserva Cancelada por Recepcion'),('Reserva Cancelada por Cliente'),('Reserva Cancelada por No-Show'),('Reserva con Ingreso')
+
+
 
 --Migracion Tabla Consumible
 SET IDENTITY_INSERT [BIG_DATA].[Consumible] ON
@@ -488,32 +217,40 @@ INSERT INTO [BIG_DATA].[Hotel] (idCiudad,direccionHotel,numeroCalle,cantidadDeEs
 	   
 
 --Migracion Tabla Reserva
-INSERT INTO [BIG_DATA].[Reserva] (codigoReserva,fecha_Reserva_Desde,cantidadNoches,idTipoHabitacion,idHotel,idRegimen,idHabitacion,idCliente,idEstadoReserva)
-	SELECT DISTINCT Reserva_Codigo,Reserva_Fecha_Inicio,Reserva_Cant_Noches,Habitacion_Tipo_Codigo,h.idHotel,r.idRegimen,hab.idHabitacion
-	from 
+INSERT INTO [BIG_DATA].[Reserva] (codigoReserva,fecha_Reserva_Desde,cantidadNoches,idTipoHabitacion,idHotel,idRegimen,idHabitacion,idCliente)
+SELECT DISTINCT Reserva_Codigo,Reserva_Fecha_Inicio,Reserva_Cant_Noches,Habitacion_Tipo_Codigo,h.idHotel,r.idRegimen,hab.idHabitacion,c.idCliente
+		from 
 		[gd_esquema].[Maestra],
 		[BIG_DATA].[TipoHabitacion],
 		[BIG_DATA].[Hotel] h,
-		--[BIG_DATA].[Regimen] r,
-		--[BIG_DATA].[Habitacion] hab,
-		--[BIG_DATA].[Cliente] c,
+		[BIG_DATA].[Regimen] r,
+		[BIG_DATA].[Habitacion] hab,
+		[BIG_DATA].[Cliente] c,
 		[BIG_DATA].[Ciudad] ciu
 		
-		
+
 	WHERE	Reserva_Codigo IS NOT NULL
 			AND (Habitacion_Tipo_Codigo=idTipoHabitacion)
 			AND (Hotel_Ciudad=ciu.ciudadNombre )
-			
-			AND	(Regimen_Descripcion=R.descripcion)
+			AND	(Regimen_Descripcion=r.descripcion)
 			AND (c.documento = Cliente_Pasaporte_Nro)
+			AND (Habitacion_Numero = hab.numeroHab)
+			AND (hab.idHotel = h.idHotel)
 
-			select * from big_data.Habitacion
-			select top 10 * from BIG_DATA.ciudad
+			
+		/*	SELECT DISTINCT Reserva_Codigo,Reserva_Fecha_Inicio,Reserva_Cant_Noches,Habitacion_Tipo_Codigo,h.idHotel,count(Reserva_Codigo) 			 			
+			
+			from	[gd_esquema].[Maestra],[BIG_DATA].[Hotel] h, [BIG_DATA].[Ciudad] ciu, [BIG_DATA].[Cliente] cli
+						
+			where Hotel_Calle = h.direccionHotel and Hotel_Nro_Calle = h.numeroCalle and Hotel_Ciudad = ciu.ciudadNombre and Cliente_Pasaporte_Nro = cli.documento  					 					
+			
+			group by Reserva_Cant_Noches,Reserva_Codigo,Reserva_Fecha_Inicio,Habitacion_Tipo_Codigo,h.idHotel
+			*/
 
 
 
 
-
+/*
 			SELECT DISTINCT Reserva_Codigo,Reserva_Fecha_Inicio,Reserva_Cant_Noches,Habitacion_Tipo_Codigo,h.idHotel,count(Reserva_Codigo)
 			
 			from	[gd_esquema].[Maestra],
@@ -553,7 +290,7 @@ INSERT INTO [BIG_DATA].[Reserva] (codigoReserva,fecha_Reserva_Desde,cantidadNoch
 	AGREGAR IDTIPOHABITACION
 	*/
 
-
+	*/
 --Migracion Tabla Estadia
 INSERT into [BIG_DATA].[Estadia](fecha_Check_In,cantidadDias)
 	SELECT m.Estadia_Fecha_Inicio,m.Estadia_Cant_Noches
